9c5e61e2b7b6bc31fd4c77cb5f6bfb226ac31a6a
Stonepaw <atchu.stonepaw@gmail.com>
2017-11-16 00:38:37 -0800

    Merge branch 'master' of https://github.com/yharaskrik/nuclear-winners-server into andrew Added random stuff for testing

--- views/__init__.py
+++ views/__init__.py
@@ -2,3 +2,4 @@ from flask import Blueprint
 views = Blueprint('views', __name__)
 
 from .jay import *
+from .andrew import *
--- views/andrew.py
+++ views/andrew.py
@@ -1,5 +1,30 @@
-from views import app
+from flask import jsonify, request, render_template
+
+from . import views as app
 
 @app.route("/product/<int:id>", methods=['GET'])
 def do_something(pid):
-    return pid
+
+    obj = {}
+
+    obj["this thing"] = pid
+
+    return jsonify(obj)
+
+
+@app.route("/product/add", methods=['post', 'get'])
+def add_product():
+
+    if request.method == 'GET':
+        return render_template("add_product.html")
+
+
+    """Creates a product from form data"""
+    data = {request.form}
+    f = request.files['image']
+    if f is not None:
+        data["image"] = True
+
+    return jsonify(data)
+
+
