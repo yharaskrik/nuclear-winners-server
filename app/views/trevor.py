15caa62b56287f2d7e4a7cae53761f3fd06a7a41
Trevor <trevorg@hotmail.ca>
2017-11-18 19:26:15 -0800

    Merge branch 'master' of https://github.com/yharaskrik/nuclear-winners-server into Trevor_Gallicano
    Conflicts:
    	requirements.txt
    	views/__init__.py

--- app/views/trevor.py
+++ app/views/trevor.py
@@ -1,8 +1,38 @@
 # @app.route('/get/Product/<type:param>')
 # def name(param)
-from flask import jsonify
-from views import views as app
+from flask import jsonify, request, render_template
+from . import get_db, app
 
-@app.route('/get/product/<int:id>')
-def something(id):
-    return jsonify(id)
+
+#allows users to create an account
+@app.route('/user/register', methods=['post','get'])
+def register_user():
+    if request.method == 'GET':
+        return render_template("register_user.html",data={})
+    data = request.form
+    #validates entered information
+    if data['username'] != data['confirmusername']:
+        return render_template('register_user.html',data=data,errormsg='Username does not match.')
+    if  data['password'] != data['confirmpassword']:
+        return render_template('register_user.html',data=data,errormsg='Password does not match.')
+    if not data['name'] or not data['username'] or not data['password'] or not data['address']:
+        return render_template('register_user.html', data=data,errormsg='All fields are required.')
+    sql = 'INSERT INTO User(name,pass,address,accountType,username) VALUES(%s,%s,%s,0,%s)'
+    try:
+        with get_db().cursor() as cursor:
+            cursor.execute(sql,[data['name'],data['password'],data['address'],data['username']])
+        return 'success'
+        #return redirect(url_for('/login'))
+    except Exception:
+        return render_template('register_user.html',data=data,errormsg='This username already exists')
+
+#allows admins to see a list of all customers
+@app.route('/admin/customers')
+def list_customers():
+    sql = 'SELECT * FROM User WHERE accountType = 0'
+    with get_db().cursor() as cursor:
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        return render_template('list_customers.html', data=data)
+    return 'picnic'
+
