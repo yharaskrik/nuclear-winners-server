782af36ed3b10a3a0ea2f27ffc00732f6357fb5e
Stonepaw <atchu.stonepaw@gmail.com>
2017-11-19 00:05:17 -0800

    Completed add product, edit product and product details Added image uploading Added admin session variable

--- app/views/andrew.py
+++ app/views/andrew.py
@@ -11,24 +11,3 @@ def view_products():
         return render_template("product_list.html", products=result)
 
 
-create_prod_sql = "INSERT INTO Product (name, description , price) VALUES (%s, %s, %s)"
-
-
-@app.route("/product/add", methods=['post', 'get'])
-def add_product():
-
-    if request.method == 'GET':
-        return render_template("add_product.html")
-
-    """Creates a product from form data"""
-    data = request.form
-    f = request.files['image']
-
-    with get_db().cursor() as cursor:
-        cursor.execute(create_prod_sql, [data["name"], data["description"], data["price"]])
-
-    return jsonify(data)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
