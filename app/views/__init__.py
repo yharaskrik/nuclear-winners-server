cd89e961f5ce28c02c526d7f68ca6a0480d69390
Mike Brehl <mikebrehl@hotmail.com>
2017-11-18 19:47:45 -0800

    Did small work on the account page, created the order history page

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -3,8 +3,8 @@ from flask import Blueprint, Flask
 from app import app
 from app import get_db
 
-views = Blueprint('views', __name__)
+views = Blueprint('views', __name__, template_folder='templates')
 
 from .andrew import *
 from .jay import *
-
+from .mike import *
--- app/views/mike.py
+++ app/views/mike.py
@@ -1,9 +1,39 @@
-from flask import jsonify
+from flask import jsonify, render_template
 
-from views import views as app
+from . import get_db, app
 
-@app.route('/mike')
-def hola_amigo():
-    obj = {}
-    obj[0] = 'strings stuff'
-    return jsonify(obj)
+
+@app.route('/account/<int:cid>')
+def view_account(cid):
+    sql = 'SELECT * FROM User WHERE id = %s'
+    with get_db().cursor() as cursor:
+        cursor.execute(sql, [cid])
+        user = cursor.fetchone()
+        if not user:
+            return render_template('no_account.html')
+        else:
+            return render_template('account.html', user=user)
+
+
+@app.route('/account/<int:cid>/orders')
+def order_history(cid):
+    sql = 'SELECT S.shipmentID, S.status, S.shippingMethodID, S.paymentMethodID, P.name, SP.quantity, (P.price*SP.quantity) AS total ' \
+          'FROM Shipment AS S, Product AS P, ShippedProduct AS SP ' \
+          'WHERE S.shipmentID = SP.shipmentID AND SP.sku = P.sku AND S.userID = %s'
+    with get_db().cursor() as cursor:
+        cursor.execute('SELECT S.shipmentID, S.status, S.shippingMethodID, S.paymentMethodID '
+                       'FROM Shipment AS S WHERE S.userID = %s', cid)
+        orders = cursor.fetchall()
+        for order in orders:
+            order['subtotal'] = 0
+            cursor.execute('SELECT P.name, SP.quantity, (P.price*SP.quantity) AS total '
+                           'FROM Product AS P, ShippedProduct AS SP '
+                           'WHERE SP.sku = P.sku AND SP.shipmentID = %s', order['shipmentID'])
+            order['products'] = cursor.fetchall()
+            for product in order['products']:
+                order['subtotal'] += product['total']
+        return render_template('order_history.html', orders=orders)
+
+
+@app.route('/cart')
+def cart():
