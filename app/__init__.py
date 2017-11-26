14b46cf4dd0b07fc639218b30ef5c6e96bdc8b65
Stonepaw <atchu.stonepaw@gmail.com>
2017-11-25 18:17:35 -0800

    Added get_db().commit() to all insert and update calls.
    This is due to changing the database engine now supporting transactions

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,7 @@ def connect_db():
                                  db=dbconfig.db,
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
+    connection.autocommit(True)
     return connection
 
 
